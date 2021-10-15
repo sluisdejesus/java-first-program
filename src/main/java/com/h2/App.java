b85e7833637408495c32623489568a0dce893f48
Sara <sluisdejesus@gmail.com>
2021-10-15 11:04:43 +0100

    more code

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -16,6 +16,10 @@ public class App
     }
 
     private static int add(int [] numbers){
-        return -1;
+        int sum = 0;
+        for (int number : numbers){
+            sum += number;
+        }
+        return sum;
     }
 }
