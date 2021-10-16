f8802ad0bec2981563924270ea994cf25afc6fa9
Sara <sluisdejesus@gmail.com>
2021-10-16 11:28:46 +0100

    parseFloat

---
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -0,0 +1,49 @@
+package com.h2;
+
+import java.time.LocalDate;
+import java.time.YearMonth;
+
+public class SavingsCalculator {
+    private float [] credits;
+    private float [] debits;
+
+    public SavingsCalculator(float [] credits, float [] debits){
+
+        this.credits = credits;
+        this.debits = debits;
+    }
+
+    private float sumOfCredits(){
+        float sum = 0.0f;
+        for (float credit : credits){
+            sum += credit;
+        }
+        return sum;
+    }
+
+    private float sumOfDebits(){
+        float sum = 0.0f;
+        for (float debit : debits){
+            sum += debit;
+        }
+        return sum;
+    }
+
+    private static int remainingDaysInMonth(LocalDate date){
+        YearMonth yearMonth = YearMonth.of(date.getYear(), date.getMonth());
+        int totalDaysInMonth = yearMonth.lengthOfMonth();
+        int remainingDays = totalDaysInMonth - date.getDayOfMonth();
+
+        return remainingDays;
+    }
+
+    public float calculate(){
+        return sumOfCredits() - sumOfDebits();
+    }
+
+    public static void main(String[] args ){
+        String[] creditsAsStrings = args[0].split(",");
+        String[] debitsAsStrings = args[0].split(",");
+
+    }
+}
