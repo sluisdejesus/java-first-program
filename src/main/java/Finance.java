8e421310f68f45b2edbbd14c9aa1383166da0e67
Sara <sluisdejesus@gmail.com>
2021-10-20 08:33:03 +0100

    module5 passing

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 import java.util.Map;
 
 public class Finance {
-
     public static final String BEST_LOAN_RATES = "bestLoanRates";
     public static final String SAVINGS_CALCULATOR = "savingsCalculator";
     public static final String MORTGAGE_CALCULATOR = "mortgageCalculator";
@@ -14,12 +13,11 @@ public class Finance {
     public static final Map<String, String> commandsToUsage = Map.of(
             BEST_LOAN_RATES, "usage: bestLoanRates",
             SAVINGS_CALCULATOR, "usage: savingsCalculator <credits separated by ','> <debits separated by ','>",
-            MORTGAGE_CALCULATOR , "usage: mortgageCalculator <loanAmount> <termInYears> <annualRate>"
+            MORTGAGE_CALCULATOR, "usage: mortgageCalculator <loanAmount> <termInYears> <annualRate>"
     );
 
-
-    private static boolean validateCommandArguments(String[] args){
-        switch (args[0]){
+    private static boolean validateCommandArguments(String[] args) {
+        switch (args[0]) {
             case BEST_LOAN_RATES:
                 return args.length == 1;
             case SAVINGS_CALCULATOR:
@@ -28,10 +26,10 @@ public class Finance {
                 return args.length == 4;
         }
         return false;
-    };
+    }
 
-    private static void executeCommand(String command, String[] arguments){
-        switch(command) {
+    private static void executeCommand(String command, String[] arguments) {
+        switch (command) {
             case BEST_LOAN_RATES:
                 System.out.println("Finding best loan rates ...");
                 BestLoanRates.main(arguments);
@@ -44,19 +42,22 @@ public class Finance {
                 System.out.println("Finding your monthly payment ...");
                 MortgageCalculator.main(arguments);
                 return;
-        };
-
         }
+    }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String command = args[0];
-        if(!commandsToUsage.containsKey(command)){
+        if(!commandsToUsage.containsKey(command)) {
             System.out.println(command + ": command not found");
             return;
         }
+
+        boolean isValidCommand = validateCommandArguments(args);
+        if(!isValidCommand) {
+            System.out.println(commandsToUsage.get(args[0]));
+            return;
+        }
+
         executeCommand(command, Arrays.copyOfRange(args, 1, args.length));
     }
-
-
-
 }
