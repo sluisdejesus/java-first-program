270149821831822c3c64b67753c9b8d96e1786ff
Sara <sluisdejesus@gmail.com>
2021-10-20 08:17:16 +0100

    trying to pass module5

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -33,7 +33,7 @@ public class Finance {
     private static void executeCommand(String command, String[] arguments){
         switch(command) {
             case BEST_LOAN_RATES:
-                System.out.println("Finding the best loan rates ...");
+                System.out.println("Finding best loan rates ...");
                 BestLoanRates.main(arguments);
                 return;
             case SAVINGS_CALCULATOR:
